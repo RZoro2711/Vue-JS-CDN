62ec479f7ffd1a689f2711f6fd85612c1757e546
Khunn Satt <kset.dkk@gmail.com>
2023-05-26 15:11:10 +0630

    lesson 4

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ const app = Vue.createApp({
             isShow : true,
         }
     },
+    methods:{
+        mouseOverHandler(event, data){
+            console.log('Mouse Over is Working', event.type, data);
+        },
+        mouseLeaveHandler(event, data){
+            console.log('Mouse Leave is Working', event.type, data);
+        },
+        doubleClickHandler(event, data){
+            console.log('Double Click is Working', event.type, data);
+        }
+    }
 })
 app.mount("#app");
