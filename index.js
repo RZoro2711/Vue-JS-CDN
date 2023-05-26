0bcaa441f61f29026eb6d3001d2336b37eec853f
Khunn Satt <kset.dkk@gmail.com>
2023-05-26 16:12:14 +0630

    lesson 6

--- index.js
+++ index.js
@@ -6,21 +6,34 @@ const app = Vue.createApp({
           title: "Book 1",
           author: "Khunn Satt Ko Ko",
           age: 21,
+          img : "images/1.png",
+          isfav : true,
         },
         {
           title: "Book 2",
           author: "Khunn Satt",
           age: 22,
+          img : "images/2.jpg",
+          isfav : false,
         },
         {
           title: "Book 3",
           author: "Zoro",
           age: 23,
+          img : "images/3.jpg",
+          isfav : true,
         },
       ],
       isShow: true,
     };
   },
+  computed:{
+    chooseClick(){
+        return this.books.filter(book => {
+            return book.isfav;
+        })
+    }
+  }
 
 });
 app.mount("#app");
